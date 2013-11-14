1d33a0c39223c7914ffb402bd738c4376756897a
rodriges17 <rodriges17@gmail.com>
2013-11-14 19:47:09 +0100

    finer grained Global.java; fix DB connection

---
+++ app/Global.java
@@ -0,0 +1,44 @@
+import play.*;
+import play.libs.*;
+import com.avaje.ebean.Ebean;
+import models.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Global extends GlobalSettings {
+
+	@Override
+    public void onStart(Application app) {
+        // Check if there is no user in the database
+        if (User.find.findRowCount() == 0) {          
+            User admin = new User("admin@test.com", "admin", true);
+            admin.save();
+        }
+        
+        User admin = User.findByEmail("admin@test.com");
+
+        // Check if there are no contact groups in the database
+        if(ContactGroup.find.findRowCount() == 0) {
+            ContactGroup bern = new ContactGroup("Bern", admin);
+            bern.save();
+            ContactGroup basel = new ContactGroup("Basel", admin);
+            basel.save();
+            ContactGroup zurich = new ContactGroup("Zürich", admin);
+            zurich.save();
+            ContactGroup tessin = new ContactGroup("Tessin", admin);
+            tessin.save();
+            ContactGroup luzern = new ContactGroup("Luzern", admin);
+            luzern.save();
+            ContactGroup romande = new ContactGroup("Romande", admin);
+            romande.save();
+            ContactGroup stgallen = new ContactGroup("St. Gallen-Zürich", admin);
+            stgallen.save();
+        }
+
+        // Check if there is no contact in the database
+        if (Contact.find.findRowCount() == 0)
+        	Contact.create("Sir", "Test", "John", "john.test@test.com", "Teststreet 3", "App1", "App2", "3012", "Test City", "012 345 6789", "Bern", "ja");             
+    }
+}
